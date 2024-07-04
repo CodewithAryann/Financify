36fd2b13b51f121b1fa6260bb2430a1722f67982
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-04 16:03:13 +0400

    7/4/24

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -15,8 +15,18 @@ function Landingpage() {
             <source src={videoFile} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
-          <h1 className='absolute uppercase text-5xl tracking-tighter leading-none font-["Montserrat"] z-20 left-[5%] font-semibold '>
+          <h1 className='absolute uppercase text-5xl tracking-tighter leading-none font-["Montserrat"] z-20 left-[5%] top-[40%] font-semibold text-white backdrop-blur-[2px] '>
             A Better Mortgage
+          </h1>
+        </div>
+        <div className="masker">
+          <h1 className='absolute uppercase text-5xl tracking-tighter leading-none font-["Montserrat"] z-20 left-[5%] top-[53%] font-semibold text-white backdrop-blur-[2px] '>
+            Experience,Without
+          </h1>
+        </div>
+        <div className="masker">
+          <h1 className='absolute uppercase text-5xl tracking-tighter leading-none font-["Montserrat"] z-20 left-[5%] top-[65%] font-semibold text-white backdrop-blur-[2px] '>
+          The Hassle.
           </h1>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='fixed z-[999] w-full pl-[3rem] pr-[3rem] py-[1.5rem] font-["Neue Montreal"] flex justify-between items-center'>
+    <div className='fixed z-[999] w-full pl-[3rem] pr-[3rem] py-[1.5rem] font-["Neue Montreal"] flex justify-between items-center text-white'>
       <div>
         <img className='w-[40%]' src="https://financify.ae/wp-content/uploads/2023/06/123456.png" alt="" />
       </div>
-      <div className='links flex gap-10'>
+      <div className='links flex gap-10 cursor-pointer'>
         {["Home","About Us","Our Services","Career","Contact Us"].map((items, index)=>(
             <a key={index} className={`text-lg capitalize font-[400] ${index=== 4 && "ml-32"}`}>{items}</a>
             ))}
