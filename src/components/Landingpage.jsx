394d69c4771d441adc7ab0938fc25b32a0e1a70e
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-04 17:54:46 +0400

    7/4/24

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -5,9 +5,9 @@ function Landingpage() {
   return (
     <div className="w-full h-screen  pt-2">
       <div className="textstructure mt-40 px-20">
-        <div className="masker">
-          <video
-            className="absolute top-0 left-0 w-full h-full object-cover z-10"
+        <div className="video">
+        <video
+            className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
             autoPlay
             loop
             muted
@@ -15,6 +15,9 @@ function Landingpage() {
             <source src={videoFile} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
+        </div>
+        <div className="bg-overlay bg-[rgba(0,0,0,.4)] w-[100%] h-[100%] absolute z-3 top-0 left-0 "></div>
+        <div className="masker">
           <h1 className='absolute uppercase text-5xl tracking-tighter leading-none font-["Montserrat"] z-20 left-[5%] top-[40%] font-semibold text-white backdrop-blur-[2px] '>
             A Better Mortgage
           </h1>
@@ -25,9 +28,12 @@ function Landingpage() {
           </h1>
         </div>
         <div className="masker">
-          <h1 className='absolute uppercase text-5xl tracking-tighter leading-none font-["Montserrat"] z-20 left-[5%] top-[65%] font-semibold text-white backdrop-blur-[2px] '>
+          <h1 className='absolute uppercase text-5xl tracking-tighter leading-none font-["Montserrat"] z-20 left-[5%] top-[65%] font-semibold text-white backdrop-blur-[1px] '>
           The Hassle.
           </h1>
+        </div>
+        <div>
+          <button>Get Started</button>
         </div>
       </div>
     </div>
