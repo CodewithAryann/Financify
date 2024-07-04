f9e8d7ae8cc6ce4c6a6d7e44306aafab1478d45c
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-04 23:04:14 +0400

    7/4/24

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -4,9 +4,8 @@ import videoFile from "../assets/video.mp4";
 function Landingpage() {
   return (
     <div className="w-full h-screen  pt-2">
-      <div className="textstructure mt-40 px-20">
-        <div className="video">
-        <video
+      <div className="video">
+          <video
             className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
             autoPlay
             loop
@@ -17,24 +16,18 @@ function Landingpage() {
           </video>
         </div>
         <div className="bg-overlay bg-[rgba(0,0,0,.4)] w-[100%] h-[100%] absolute z-3 top-0 left-0 "></div>
-        <div className="masker">
-          <h1 className='absolute uppercase text-5xl tracking-tighter leading-none font-["Montserrat"] z-20 left-[5%] top-[40%] font-semibold text-white backdrop-blur-[2px] '>
-            A Better Mortgage
-          </h1>
-        </div>
-        <div className="masker">
-          <h1 className='absolute uppercase text-5xl tracking-tighter leading-none font-["Montserrat"] z-20 left-[5%] top-[53%] font-semibold text-white backdrop-blur-[2px] '>
-            Experience,Without
-          </h1>
-        </div>
-        <div className="masker">
-          <h1 className='absolute uppercase text-5xl tracking-tighter leading-none font-["Montserrat"] z-20 left-[5%] top-[65%] font-semibold text-white backdrop-blur-[1px] '>
-          The Hassle.
-          </h1>
-        </div>
-        <div>
-          <button>Get Started</button>
-        </div>
+      <div className="textstructure mt-[12rem] px-[3.5rem] leading-7">
+        {["A Better Mortgage", "Experience, Without","The Hassle."].map(
+          (item, index) => {
+            return (
+              <div className="masker">
+                <h1 className=' uppercase text-5xl tracking-tighter leading-[3.4rem] font-["Montserrat"]  font-semibold text-white backdrop-blur-[2px] '>
+                  {item}
+                </h1>
+              </div>
+            );
+          }
+        )}
       </div>
     </div>
   );
