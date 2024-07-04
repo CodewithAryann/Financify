2af3b93f743fc40f3ab1c75a9d2d76ad96e4d46f
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-04 15:52:30 +0400

    7/4/24

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
+import videoFile from "../assets/video.mp4";
 
 function Landingpage() {
   return (
-    <div className='w-full h-screen bg-zinc-50 pt-2'>
-        <div className='textstructure mt-40 px-20'>
-            <div className='masker'>
-                <video src=""></video>
-                <h1 className='uppercase text-7xl leading-none font-["Montserrat"] '>A Better Mortgage</h1>
-            </div>
+    <div className="w-full h-screen  pt-2">
+      <div className="textstructure mt-40 px-20">
+        <div className="masker">
+          <video
+            className="absolute top-0 left-0 w-full h-full object-cover z-10"
+            autoPlay
+            loop
+            muted
+          >
+            <source src={videoFile} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <h1 className='absolute uppercase text-5xl tracking-tighter leading-none font-["Montserrat"] z-20 left-[5%] font-semibold '>
+            A Better Mortgage
+          </h1>
         </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default Landingpage
+export default Landingpage;
