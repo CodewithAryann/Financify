34219761930cf129ec5d83a1f58aab2bea9c4672
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-04 15:26:20 +0400

    7/4/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Landingpage from './components/Landingpage'
 
 function App() {
   return (
-    <div className='w-screen h-screen bg-white text-black'>
+    <div className='w-screen h-screen  text-black'>
       <Navbar />
+      <Landingpage/>
+      
     </div>
     
   )
---
+++ src/components/Landingpage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Landingpage() {
+  return (
+    <div className='w-full h-screen bg-zinc-50 pt-2'>
+        <div className='textstructure mt-40 px-20'>
+            <div className='masker'>
+                <h1 className='uppercase text-7xl leading-none font-["Montserrat"] '>A Better Mortgage</h1>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Landingpage
