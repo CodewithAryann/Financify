e57bdc5d11058c820479fa46be8f773cf598eab5
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-05 23:21:06 +0400

    7/5/24

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='w-full py-20 bg-black text-white  rounded-tl-3xl rounded-tr-3xl'>
-      <h1>Financify is a trusted name in the UAE mortgage industry, offering a personalized approach to secure the best possible mortgage for its clients. At Financify, customer is at the heart of everything we do. We take pride in our commitment to build lasting relationships with our clients, which will be evident with our exceptional customer service.</h1>
+    <div className='w-full p-20 bg-black text-white  rounded-tl-3xl rounded-tr-3xl'>
+      <h1 className='font-["Montserrat"] text-3xl leading-[3.5vw] tracking-tighter'>Financify is a trusted name in the UAE mortgage industry, offering a personalized approach to secure the best possible mortgage for its clients. At Financify, customer is at the heart of everything we do. We take pride in our commitment to build lasting relationships with our clients, which will be evident with our exceptional customer service.</h1>
     </div>
-  )
+  );
 }
 
 export default About
