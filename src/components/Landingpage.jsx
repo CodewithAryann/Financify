bbf097ce011ec34c52d63615f0ca048024153c42
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-05 16:39:05 +0400

    7/5/24

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { FaArrowUp } from "react-icons/fa6";
 import videoFile from "../assets/video.mp4";
 
 function Landingpage() {
   return (
     <div className="w-full h-screen  pt-2">
       <div className="video">
-          <video
-            className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
-            autoPlay
-            loop
-            muted
-          >
-            <source src={videoFile} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        <div className="bg-overlay bg-[rgba(0,0,0,.4)] w-[100%] h-[100%] absolute z-3 top-0 left-0 "></div>
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+          autoPlay
+          loop
+          muted
+        >
+          <source src={videoFile} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="bg-overlay bg-[rgba(0,0,0,.4)] w-[100%] h-[100%] absolute z-3 top-0 left-0 "></div>
       <div className="textstructure mt-[12rem] px-[3.4rem] leading-7">
-        {["A Better Mortgage", "Experience, Without","The Hassle."].map(
+        {["A Better Mortgage", "Experience, Without", "The Hassle."].map(
           (item, index) => {
             return (
               <div className="masker">
@@ -29,8 +30,13 @@ function Landingpage() {
           }
         )}
       </div>
-      <div className="Get-start">
+      <div className="Get-start flex">
         <button class="btn">Get Started</button>
+        <div className="mt-5 arrow w-[58px] h-[57px] flex items-center justify-center rounded-full ">
+          <span className="rotate-[45deg]">
+            <FaArrowUp />
+          </span>
+        </div>
       </div>
       {/* <div className="border-t-[1px] border-zinc-100 mt-10"></div> */}
     </div>
