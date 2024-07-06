604f7991d1684b934c464856e535bf5a16a90f43
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-06 23:21:21 +0400

    7/6/24

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Eyes() {
   return (
-    <div className='eyes w-full h-screen overflow-hidden'>
-      <div className='relative w-full h-full bg-cover bg-center bg-[url("src/assets/play.png")]'></div>
-      <div className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-1/2 h-96 bg-red-500  '></div>
+    <div className="eyes w-full h-screen overflow-hidden">
+      <div className='relative w-full h-full bg-cover bg-center bg-[url("src/assets/play.png")]'>
+        <div className="absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] ">
+          <div className=" flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-white ">
+          <div className="w-2/3 h-2/3 rounded-full bg-zinc-900 "></div>
+          </div>
+          <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-white ">
+          <div className="w-2/3 h-2/3 rounded-full bg-zinc-900 "></div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Eyes
+export default Eyes;
