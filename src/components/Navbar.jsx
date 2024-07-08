527fc9c7b3db5caa3ff672bfe3a69c8b93007ed4
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-08 18:03:18 +0400

    7/8/24

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
         <img className='w-[8rem]' src="https://financify.ae/wp-content/uploads/2023/06/123456.png" alt="" />
       </div>
       <div className='links flex gap-10 cursor-pointer'>
-        {["Home","About Us","Our Services","Career","Contact Us"].map((items, index)=>(
+        {["Home","About Us","Our Services","Contact Us","Mortgage Calculator"].map((items, index)=>(
             <a key={index} className={`text-lg capitalize font-[400] link-hover ${index=== 4 && "ml-32"}`}>{items}</a>
             ))}
 
