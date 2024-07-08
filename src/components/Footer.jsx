a63a4b607fe4fb024b82600f48a575f47e94aa88
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-08 21:15:32 +0400

    7/8/24

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { FaLocationDot } from "react-icons/fa6";
+import { IoIosMail } from "react-icons/io";
+import { FaPhoneAlt } from "react-icons/fa";
 
 function Footer() {
   return (
     <div className='footer w-full h-screen flex gap-10 p-10 pl-20 pr-20'>
       <div className='w-[16vw]'>
-        <img src="https://financify.ae/wp-content/uploads/2023/06/123456.png" alt="" />
-        <i><FaLocationDot /></i>
+        <img className='mb-10' src="https://financify.ae/wp-content/uploads/2023/06/123456.png" alt="" />
+        <div className='flex  gap-2'>
+        <i className='mt-1'><FaLocationDot /></i>
+        <p>Office # 402C, Aspin<br />Commercial Tower, Sheikh <br />Zayed Road, Dubai</p>
+        </div>
+        <div className='flex items-center'><i><IoIosMail /></i><a href="">info@financify.ae</a></div>
+        <div><FaPhoneAlt /></div>
+        
 
       </div>
       <div className='flex gap-20'>
