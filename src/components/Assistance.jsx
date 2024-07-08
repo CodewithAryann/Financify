e4d2e22caac5bd517baa4d5e0125ff5c82edbfb8
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-08 15:09:40 +0400

    7/8/24

--- src/components/Assistance.jsx
+++ src/components/Assistance.jsx
@@ -4,8 +4,9 @@ function Assistance() {
   return (
     <div className='pr-20 pl-20'>
         <div style={{backgroundSize: `100% 100%`,
-        padding:` 70px 0px !important`}} className='need-help w-full h-screen Assistance bg-[url("https://financify.ae/wp-content/uploads/2023/06/Rectangle-1687@2x.png")]'>
+        padding:` 70px 0px !important`}} className='need-help relativ overflow-hidden w-full h-screen Assistance bg-[url("https://financify.ae/wp-content/uploads/2023/06/Rectangle-1687@2x.png")]'>
         <div className='help '></div>
+        <div className='help-2 overflow-hidden'></div>
 
         </div>
 
