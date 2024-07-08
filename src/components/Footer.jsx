b70fa9f1eb618eca88a70db708ed47bac49e2127
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-07-08 21:21:08 +0400

    7/8/24

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,16 @@ import { FaPhoneAlt } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className='footer w-full h-screen flex gap-10 p-10 pl-20 pr-20'>
+    <div className='footer w-full h-screen flex gap-10 p-10 pl-20 pr-20 font-["Montserrat"] '>
       <div className='w-[16vw]'>
         <img className='mb-10' src="https://financify.ae/wp-content/uploads/2023/06/123456.png" alt="" />
-        <div className='flex  gap-2'>
+        <div className='flex  gap-2 mb-5'>
         <i className='mt-1'><FaLocationDot /></i>
         <p>Office # 402C, Aspin<br />Commercial Tower, Sheikh <br />Zayed Road, Dubai</p>
         </div>
-        <div className='flex items-center'><i><IoIosMail /></i><a href="">info@financify.ae</a></div>
-        <div><FaPhoneAlt /></div>
+        <div className='flex items-center gap-2 mb-5'><i><IoIosMail /></i><a href="">info@financify.ae</a></div>
+        <div className='flex gap-2'><i className='mt-2'><FaPhoneAlt /></i><a href="">+971585899398</a></div>
+        <a className='ml-6' href="">+971585655007</a>
         
 
       </div>
